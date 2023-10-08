fb6a783162c30563aad83f580dc25d1da55e20bc
BE7nHgA6 <145891488+BE7nHgA6@users.noreply.github.com>
2023-10-08 09:38:01 +0530

    Update main (5).py

--- main (5).py
+++ main (5).py
@@ -1,12 +1,12 @@
-def linearSearchProduct                  (productList, targetProduct):
+def linearSearchProduct (productList, targetProduct):
  indices=[ ]
- for index, product in                 enumerate(productList): 
+ for index, product in enumerate(productList): 
   if product == targetProduct:                                            
    indices.append(index)
  return indices
 # Example usage:
-products=["shoes","boot","loafer",  "shoes","sandal","shoes"]
+products=["shoes","boot","loafer","shoes","sandal","shoes"]
 target = "shoes"
 target2='apple'
-result=linearSearchProduct(products                           ,target)
+result=linearSearchProduct(products,target)
 print(result)
